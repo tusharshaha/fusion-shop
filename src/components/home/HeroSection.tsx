23f93bd972dafdb2da339134f93f328de39ddbd3
tusharshaha <tusharshaha128@gmail.com>
2024-12-07 10:08:20 +0600

    UI: created hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="bg-gray-100 pb-10 pt-10 md:pt-20 h-screen">
+    <div className="bg-gray-100 pb-10 pt-10 md:pt-20 h-[350px] md:h-screen hero-banner">
       <div className="cus-container text-center justify-content-center">
-        <h1 className="header">Welcome to <span className="font-bold">Fushion Shop</span></h1>
+        <h1 className="header">
+          Welcome to <span className="font-bold">Fushion Shop</span>
+        </h1>
         <p className="">Simple and minimal ecommerce website</p>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,8 @@ export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       colors: {},
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
